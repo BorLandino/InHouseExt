0c3e42698e7f05e2411e9f0913568288977163da
BorLandino <21695274+BorLandino@users.noreply.github.com>
2022-08-07 13:48:17 +0300

    Create InHouse.js

---
+++ InHouse.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name         InHouseExt
+// @version      1
+// @description  try to take over the world!
+// @author       vlgom
+// @match        https://ikeainhousedelivery.azurewebsites.net/Shipment/PreparedShipment
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @require      https://raw.githubusercontent.com/BorLandino/InHouseExt/main/InHouseExt.js
+// @grant        none
+// ==/UserScript==
