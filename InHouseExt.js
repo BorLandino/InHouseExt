92f856d5395b2cc2081c3befd0d2e83d840fd838
BorLandino <21695274+BorLandino@users.noreply.github.com>
2022-08-28 11:52:44 +0300

    Update InHouseExt.js

--- InHouseExt.js
+++ InHouseExt.js
@@ -45,7 +45,7 @@ function main() {
     MEMO = getCookie('MEMO');
     console.log(MEMO);
     MEMO = prompt('Введите ваш MEMO для запуска скрипта(0.7.6)',MEMO);
-    setCookie("MEMO",MEMO,{'max-age': 36000});
+    setCookie("MEMO",MEMO,{'max-age': 3600});
     if (MEMO != null && MEMO != "") {
         //обновляем ссылки при первом запуске
         timeoutUpdateLinks();
